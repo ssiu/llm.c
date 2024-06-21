a22762f74a8a1a18d5929aa6ea2f256e74783cf9
Steve Siu <steve.siu@oracle.com>
2024-06-21 22:41:22 +1000

    fix

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -717,7 +717,7 @@ __global__ void __launch_bounds__(16*16, 2) matmul_forward_kernel4(float* out,
 
 __global__ __launch_bounds__(256,2)
 void matmul_forward_kernel5(float* out,
-                     float* inp, float* weight, float* bias,
+                     const float* inp, const float* weight, const float* bias,
                      int B, int T, int C, int OC){
 
     assert(B * T % 128  == 0);
