71426409ba5433f6db7918d6fdffa436d786984e
Steve Siu <steve.siu@oracle.com>
2024-07-13 23:11:36 +1000

    fix

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -1110,7 +1110,7 @@ void fused_matmul_forward_gelu_kernel(float* out_gelu, float* out,
 
 }
 
-#include <iostream>
+
 __global__ void matmul_backward_kernel1(float* A, float* B, float* dinp, int BT, int C, int OC){
     int bx = blockIdx.x;
     int by = blockIdx.y;
@@ -1129,131 +1129,131 @@ __global__ void matmul_backward_kernel1(float* A, float* B, float* dinp, int BT,
 }
 
 
-#define A(i,j) A[(i) * BT + (j)]
-#define B(i,j) B[(i) * OC + (j)]
-#define dinp(i,j) dinp[(i) * BT + (j)]
-#define sA(pointer, i,j) sA[(pointer)][((i) << 7) + (j)]
-#define sB(pointer, i,j) sB[(pointer)][((i) << 7) + (j)]
-#define TILE_WIDTH 128
-#define BLOCK_WIDTH 8
-#define FLOAT_4(pointer) reinterpret_cast<float4*>(&(pointer))[0]
-
-__global__ __launch_bounds__(256)
-void matmul_backward_kernel2(float* A, float* B, float* dinp, int BT, int C, int OC){
-    int thread_id = threadIdx.x;
-    int block_idx = blockIdx.x;
-    int block_idy = blockIdx.y;
-    int warp_id = threadIdx.x >> 5;
-    int lane_id = threadIdx.x & 31;
-    int warp_row = (warp_id >> 1) << 5;
-
-    int warp_col = (warp_id & 1) << 6;
-    int thread_row = (lane_id >> 3) << 2;
-    int thread_col = (lane_id & 7) << 2;
-
-    int sA_row = thread_id >> 1;
-    int sA_col = (thread_id & 1) << 2;
-
-    int sB_row = thread_id >> 5;
-    int sB_col = (thread_id & 31) << 2;
-
-    int C_row = warp_row + thread_row;
-    int C_col = warp_col + thread_col;
-
-    A = &A((block_idx << 7), 0);
-    B = &B(0, (block_idy << 7));
-    dinp = &dinp((block_idx << 7), (block_idy << 7));
-
-    __shared__ float sA[2][BLOCK_WIDTH * TILE_WIDTH];
-    __shared__ float sB[2][BLOCK_WIDTH * TILE_WIDTH];
-
-
-    float rA[4];
-    float rB[4];
-
-    float fA[8] = {};
-    float fB[8] = {};
-
-    float accum[64] = {};
-
-    int shared_pointer = 0;
-    // load first block
-    FLOAT_4(rA) = FLOAT_4(A(sA_row, sA_col));
-    FLOAT_4(rB) = FLOAT_4(B(sB_row, sB_col));
-    #pragma unroll
-    for (int i=0; i<4;i++){
-        sA(shared_pointer, sA_col + i, sA_row) = rA[i];
-        // sA[shared_pointer][sA_sOffset + i*TILE_WIDTH] = rA[i];
-    }
-
-    FLOAT_4(sB(shared_pointer, sB_row, sB_col)) = FLOAT_4(rB);
-
-    __syncthreads();
-
-    A += BLOCK_WIDTH;
-    B += BLOCK_WIDTH * OC;
-
-    for (int kBlock=0; kBlock< OC / BLOCK_WIDTH; kBlock++){
-
-        // load from gmem A, B for next block
-        if (kBlock < OC/BLOCK_WIDTH - 1) {
-            FLOAT_4(rA) = FLOAT_4(A(sA_row, sA_col));
-            FLOAT_4(rB) = FLOAT_4(B(sB_row, sB_col));
-        }
-        #pragma unroll
-        for (int kFragment=0; kFragment<BLOCK_WIDTH; kFragment++) {
-            // load from smem A, B
-            FLOAT_4(fA[0]) = FLOAT_4(sA(shared_pointer, kFragment, C_row));
-            FLOAT_4(fA[4]) = FLOAT_4(sA(shared_pointer, kFragment, C_row + 16));
-            FLOAT_4(fB[0]) = FLOAT_4(sB(shared_pointer, kFragment, C_col));
-            FLOAT_4(fB[4]) = FLOAT_4(sB(shared_pointer, kFragment, C_col + 32));
-
-            // compute outer product
-            #pragma unroll
-            for (int i=0; i<8;i++){
-                #pragma unroll
-                for (int j=0; j<8; j++) {
-                    accum[i*8+j] += fA[i] * fB[j];
-                }
-             }
-        }
-
-        // store to smem sA, sB for next block
-        if (kBlock < OC / BLOCK_WIDTH - 1) {
-
-
-            //FLOAT_4(sA[sA_sOffset]) = FLOAT_4(rA);
-            #pragma unroll
-            for (int i=0; i<4;i++){
-                sA(shared_pointer^1, sA_col + i, sA_row) = rA[i];
-                //sA[shared_pointer^1][sA_sOffset + i*TILE_WIDTH] = rA[i];
-            }
-
-            FLOAT_4(sB(shared_pointer^1, sB_row, sB_col)) = FLOAT_4(rB);
-
-            __syncthreads();
-
-            A += BLOCK_WIDTH;
-            B += BLOCK_WIDTH * OC;
-
-            shared_pointer ^= 1;
-        }
-
-    }
-
-//    storeToGmem_5(accum, C, N, C_gOffset);
-
-    // store to gmem C
-    #pragma unroll
-    for (int i=0;i<4;i++) {
-
-        FLOAT_4(dinp(C_row + i, C_col)) = FLOAT_4(accum[i * 8]);
-        FLOAT_4(dinp(C_row + i, C_col + 32)) = FLOAT_4(accum[i * 8 + 4]);
-        FLOAT_4(dinp(C_row + i + 16, C_col)) = FLOAT_4(accum[(i+4) * 8]);
-        FLOAT_4(dinp(C_row + i + 16, C_col + 32)) = FLOAT_4(accum[(i+4) * 8 + 4]);
-
-    }
-}
+//#define A(i,j) A[(i) * BT + (j)]
+//#define B(i,j) B[(i) * OC + (j)]
+//#define dinp(i,j) dinp[(i) * BT + (j)]
+//#define sA(pointer, i,j) sA[(pointer)][((i) << 7) + (j)]
+//#define sB(pointer, i,j) sB[(pointer)][((i) << 7) + (j)]
+//#define TILE_WIDTH 128
+//#define BLOCK_WIDTH 8
+//#define FLOAT_4(pointer) reinterpret_cast<float4*>(&(pointer))[0]
+//
+//__global__ __launch_bounds__(256)
+//void matmul_backward_kernel2(float* A, float* B, float* dinp, int BT, int C, int OC){
+//    int thread_id = threadIdx.x;
+//    int block_idx = blockIdx.x;
+//    int block_idy = blockIdx.y;
+//    int warp_id = threadIdx.x >> 5;
+//    int lane_id = threadIdx.x & 31;
+//    int warp_row = (warp_id >> 1) << 5;
+//
+//    int warp_col = (warp_id & 1) << 6;
+//    int thread_row = (lane_id >> 3) << 2;
+//    int thread_col = (lane_id & 7) << 2;
+//
+//    int sA_row = thread_id >> 1;
+//    int sA_col = (thread_id & 1) << 2;
+//
+//    int sB_row = thread_id >> 5;
+//    int sB_col = (thread_id & 31) << 2;
+//
+//    int C_row = warp_row + thread_row;
+//    int C_col = warp_col + thread_col;
+//
+//    A = &A((block_idx << 7), 0);
+//    B = &B(0, (block_idy << 7));
+//    dinp = &dinp((block_idx << 7), (block_idy << 7));
+//
+//    __shared__ float sA[2][BLOCK_WIDTH * TILE_WIDTH];
+//    __shared__ float sB[2][BLOCK_WIDTH * TILE_WIDTH];
+//
+//
+//    float rA[4];
+//    float rB[4];
+//
+//    float fA[8] = {};
+//    float fB[8] = {};
+//
+//    float accum[64] = {};
+//
+//    int shared_pointer = 0;
+//    // load first block
+//    FLOAT_4(rA) = FLOAT_4(A(sA_row, sA_col));
+//    FLOAT_4(rB) = FLOAT_4(B(sB_row, sB_col));
+//    #pragma unroll
+//    for (int i=0; i<4;i++){
+//        sA(shared_pointer, sA_col + i, sA_row) = rA[i];
+//        // sA[shared_pointer][sA_sOffset + i*TILE_WIDTH] = rA[i];
+//    }
+//
+//    FLOAT_4(sB(shared_pointer, sB_row, sB_col)) = FLOAT_4(rB);
+//
+//    __syncthreads();
+//
+//    A += BLOCK_WIDTH;
+//    B += BLOCK_WIDTH * OC;
+//
+//    for (int kBlock=0; kBlock< OC / BLOCK_WIDTH; kBlock++){
+//
+//        // load from gmem A, B for next block
+//        if (kBlock < OC/BLOCK_WIDTH - 1) {
+//            FLOAT_4(rA) = FLOAT_4(A(sA_row, sA_col));
+//            FLOAT_4(rB) = FLOAT_4(B(sB_row, sB_col));
+//        }
+//        #pragma unroll
+//        for (int kFragment=0; kFragment<BLOCK_WIDTH; kFragment++) {
+//            // load from smem A, B
+//            FLOAT_4(fA[0]) = FLOAT_4(sA(shared_pointer, kFragment, C_row));
+//            FLOAT_4(fA[4]) = FLOAT_4(sA(shared_pointer, kFragment, C_row + 16));
+//            FLOAT_4(fB[0]) = FLOAT_4(sB(shared_pointer, kFragment, C_col));
+//            FLOAT_4(fB[4]) = FLOAT_4(sB(shared_pointer, kFragment, C_col + 32));
+//
+//            // compute outer product
+//            #pragma unroll
+//            for (int i=0; i<8;i++){
+//                #pragma unroll
+//                for (int j=0; j<8; j++) {
+//                    accum[i*8+j] += fA[i] * fB[j];
+//                }
+//             }
+//        }
+//
+//        // store to smem sA, sB for next block
+//        if (kBlock < OC / BLOCK_WIDTH - 1) {
+//
+//
+//            //FLOAT_4(sA[sA_sOffset]) = FLOAT_4(rA);
+//            #pragma unroll
+//            for (int i=0; i<4;i++){
+//                sA(shared_pointer^1, sA_col + i, sA_row) = rA[i];
+//                //sA[shared_pointer^1][sA_sOffset + i*TILE_WIDTH] = rA[i];
+//            }
+//
+//            FLOAT_4(sB(shared_pointer^1, sB_row, sB_col)) = FLOAT_4(rB);
+//
+//            __syncthreads();
+//
+//            A += BLOCK_WIDTH;
+//            B += BLOCK_WIDTH * OC;
+//
+//            shared_pointer ^= 1;
+//        }
+//
+//    }
+//
+////    storeToGmem_5(accum, C, N, C_gOffset);
+//
+//    // store to gmem C
+//    #pragma unroll
+//    for (int i=0;i<4;i++) {
+//
+//        FLOAT_4(dinp(C_row + i, C_col)) = FLOAT_4(accum[i * 8]);
+//        FLOAT_4(dinp(C_row + i, C_col + 32)) = FLOAT_4(accum[i * 8 + 4]);
+//        FLOAT_4(dinp(C_row + i + 16, C_col)) = FLOAT_4(accum[(i+4) * 8]);
+//        FLOAT_4(dinp(C_row + i + 16, C_col + 32)) = FLOAT_4(accum[(i+4) * 8 + 4]);
+//
+//    }
+//}
 
 // ----------------------------------------------------------------------------
 // kernel launchers
@@ -1399,7 +1399,7 @@ void matmul_backward(float* dinp, float* dweight, float* dbias,
     //cublasCheck(cublasSgemm(cublas_handle, CUBLAS_OP_N, CUBLAS_OP_N, C, B*T, OC, &one, weight, C, dout, OC, &zero, dinp, C));
 
     dim3 gridDim(B * T / 32, C / 32);
-    dim3 blockDim(TILE_WIDTH,TILE_WIDTH);
+    dim3 blockDim(32, 32);
     matmul_backward_kernel1<<<gridDim, blockDim>>>(dout, weight, dinp, B * T, C, OC);
 
 //    dim3 gridDim(B * T / 128, C / 128);
