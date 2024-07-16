49db399e9662f6005919425d6e4c30a08438da44
Steve Siu <steve.siu@oracle.com>
2024-07-16 15:16:55 +1000

    fix

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -1373,20 +1373,19 @@ void fused_matmul_gelu_backward_kernel(float* A, float* B, float* dinp, float* i
     }
 
     // load inp
-//    float reg_inp[64] = {};
-//
-//    #pragma unroll
-//    for (int i=0;i<4;i++) {
-//        FLOAT_4(reg_inp[i * 8]) = FLOAT_4(inp(C_row + i, C_col));
-//        FLOAT_4(reg_inp[i * 8 + 4]) = FLOAT_4(inp(C_row + i, C_col + 32));
-//        FLOAT_4(reg_inp[(i + 4) * 8]) = FLOAT_4(inp(C_row + i + 16, C_col));
-//        FLOAT_4(reg_inp[(i + 4) * 8 + 4]) = FLOAT_4(inp(C_row + i + 16, C_col + 32));
-//        accum[i * 8] += 1;
-//        epilogue_gelu_backward(&accum[i * 8], &reg_inp[i * 8]);
-//        epilogue_gelu_backward(&accum[i * 8 + 4], &reg_inp[i * 8 + 4]);
-//        epilogue_gelu_backward(&accum[(i + 4) * 8], &reg_inp[(i + 4) * 8]);
-//        epilogue_gelu_backward(&accum[(i + 4) * 8 + 4], &reg_inp[(i + 4) * 8 + 4]);
-//    }
+    float reg_inp[64] = {};
+
+    #pragma unroll
+    for (int i=0;i<4;i++) {
+        FLOAT_4(reg_inp[i * 8]) = FLOAT_4(inp(C_row + i, C_col));
+        FLOAT_4(reg_inp[i * 8 + 4]) = FLOAT_4(inp(C_row + i, C_col + 32));
+        FLOAT_4(reg_inp[(i + 4) * 8]) = FLOAT_4(inp(C_row + i + 16, C_col));
+        FLOAT_4(reg_inp[(i + 4) * 8 + 4]) = FLOAT_4(inp(C_row + i + 16, C_col + 32));
+        epilogue_gelu_backward(&accum[i * 8], &reg_inp[i * 8]);
+        epilogue_gelu_backward(&accum[i * 8 + 4], &reg_inp[i * 8 + 4]);
+        epilogue_gelu_backward(&accum[(i + 4) * 8], &reg_inp[(i + 4) * 8]);
+        epilogue_gelu_backward(&accum[(i + 4) * 8 + 4], &reg_inp[(i + 4) * 8 + 4]);
+    }
 
 //    storeToGmem_5(accum, C, N, C_gOffset);
 
@@ -2187,14 +2186,8 @@ void gpt2_backward(GPT2 *model) {
 
         // backprop this layer
         fused_matmul_gelu_backward(dl_bt4c, dl_fcprojw, dl_fcprojb, dresidual, l_fch_gelu, l_fcprojw, l_fch, B, T, 4*C, C);
-        for (int i=0; i<10; i++){
-            printf("%f", dl_bt4c[i]);
-        }
 //        matmul_backward(dl_bt4c, dl_fcprojw, dl_fcprojb, dresidual, l_fch_gelu, l_fcprojw, B, T, 4*C, C);
         gelu_backward(dl_bt4c, l_fch, dl_bt4c, B*T*4*C);
-        for (int i=0; i<10; i++){
-            printf("%f", dl_bt4c[i]);
-        }
         matmul_backward(dl_btc, dl_fcw, dl_fcb, dl_bt4c, l_ln2, l_fcw, B, T, C, 4 * C);
         // layernorm backward does += to the dresidual, so it correctly accumulates grad from the MLP block above
         layernorm_backward(dresidual, dl_ln2w, dl_ln2b, dl_btc, l_residual2, l_ln2w, l_ln2_mean, l_ln2_rstd, B, T, C);
