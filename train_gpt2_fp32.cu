38a58f0c0f5309e6e278204045eaedb57db7338a
Steve Siu <steve.siu@oracle.com>
2024-07-16 14:11:04 +1000

    fix

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -1372,20 +1372,20 @@ void fused_matmul_gelu_backward_kernel(float* A, float* B, float* dinp, float* i
 
     }
 
-//    // load inp
-//    float reg_inp[64] = {};
-//
-//    #pragma unroll
-//    for (int i=0;i<4;i++) {
-//        FLOAT_4(reg_inp[i * 8]) = FLOAT_4(inp(C_row + i, C_col));
-//        FLOAT_4(reg_inp[i * 8 + 4]) = FLOAT_4(inp(C_row + i, C_col + 32));
-//        FLOAT_4(reg_inp[(i + 4) * 8]) = FLOAT_4(inp(C_row + i + 16, C_col));
-//        FLOAT_4(reg_inp[(i + 4) * 8 + 4]) = FLOAT_4(inp(C_row + i + 16, C_col + 32));
-//        epilogue_gelu_backward(&accum[i * 8], &reg_inp[i * 8]);
-//        epilogue_gelu_backward(&accum[i * 8 + 4], &reg_inp[i * 8 + 4]);
-//        epilogue_gelu_backward(&accum[(i + 4) * 8], &reg_inp[(i + 4) * 8]);
-//        epilogue_gelu_backward(&accum[(i + 4) * 8 + 4], &reg_inp[(i + 4) * 8 + 4]);
-//    }
+    // load inp
+    float reg_inp[64] = {};
+
+    #pragma unroll
+    for (int i=0;i<4;i++) {
+        FLOAT_4(reg_inp[i * 8]) = FLOAT_4(inp(C_row + i, C_col));
+        FLOAT_4(reg_inp[i * 8 + 4]) = FLOAT_4(inp(C_row + i, C_col + 32));
+        FLOAT_4(reg_inp[(i + 4) * 8]) = FLOAT_4(inp(C_row + i + 16, C_col));
+        FLOAT_4(reg_inp[(i + 4) * 8 + 4]) = FLOAT_4(inp(C_row + i + 16, C_col + 32));
+        epilogue_gelu_backward(&accum[i * 8], &reg_inp[i * 8]);
+        epilogue_gelu_backward(&accum[i * 8 + 4], &reg_inp[i * 8 + 4]);
+        epilogue_gelu_backward(&accum[(i + 4) * 8], &reg_inp[(i + 4) * 8]);
+        epilogue_gelu_backward(&accum[(i + 4) * 8 + 4], &reg_inp[(i + 4) * 8 + 4]);
+    }
 
 //    storeToGmem_5(accum, C, N, C_gOffset);
 
