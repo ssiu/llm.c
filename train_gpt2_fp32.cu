b5ba42a289398a4fa7cb092215040db8eea3fbf0
Steve Siu <steve.siu@oracle.com>
2024-07-21 21:24:42 +1000

    fix

--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -1548,6 +1548,175 @@ void fused_matmul_gelu_backward_kernel2(float* A, float* B, float* dinp, float* 
 
 }
 
+
+__global__ __launch_bounds__(256,2)
+void fused_matmul_gelu_backward_kernel3(float* A, float* B, float* dinp, float* inp, int BT, int C, int OC){
+    int thread_id = threadIdx.x;
+    int block_idx = blockIdx.x;
+    int block_idy = blockIdx.y;
+
+    int warp_id = threadIdx.x >> 5;
+    int lane_id = threadIdx.x & 31;
+    int warp_row = (warp_id >> 1) << 5;
+
+    int warp_col = (warp_id & 1) << 6;
+    int thread_row = (lane_id >> 3) << 2;
+    int thread_col = (lane_id & 7) << 2;
+
+    int sA_row = thread_id >> 1;
+    int sA_col = (thread_id & 1) << 2;
+
+    int sB_row = thread_id >> 5;
+    int sB_col = (thread_id & 31) << 2;
+
+    int C_row = warp_row + thread_row;
+    int C_col = warp_col + thread_col;
+
+    A = &A((block_idx << 7), 0);
+    B = &B(0, (block_idy << 7));
+    dinp = &dinp((block_idx << 7), (block_idy << 7));
+
+    __shared__ float sA[2][TILE_WIDTH * BLOCK_WIDTH];
+    __shared__ float sB[2][TILE_WIDTH * BLOCK_WIDTH];
+
+
+    float rA[4];
+    float rB[4];
+
+    float fA[8] = {};
+    float fB[8] = {};
+
+    float accum[64] = {};
+
+    int shared_pointer = 0;
+    // load first block
+    FLOAT_4(rA) = FLOAT_4(A(sA_row, sA_col));
+    FLOAT_4(rB) = FLOAT_4(B(sB_row, sB_col));
+    #pragma unroll
+    for (int i=0; i<4;i++){
+        sA(shared_pointer, sA_col + i, sA_row) = rA[i];
+        // sA[shared_pointer][sA_sOffset + i*BLOCK_WIDTH] = rA[i];
+    }
+
+    FLOAT_4(sB(shared_pointer, sB_row, sB_col)) = FLOAT_4(rB);
+
+    __syncthreads();
+
+    A += TILE_WIDTH;
+    B += TILE_WIDTH * C;
+
+    for (int kBlock=0; kBlock < OC / TILE_WIDTH; kBlock++){
+
+        // load from gmem A, B for next block
+        if (kBlock < OC/TILE_WIDTH - 1) {
+            FLOAT_4(rA) = FLOAT_4(A(sA_row, sA_col));
+            FLOAT_4(rB) = FLOAT_4(B(sB_row, sB_col));
+        }
+        #pragma unroll
+        for (int kFragment=0; kFragment<TILE_WIDTH; kFragment++) {
+            // load from smem A, B
+            FLOAT_4(fA[0]) = FLOAT_4(sA(shared_pointer, kFragment, C_row));
+            FLOAT_4(fA[4]) = FLOAT_4(sA(shared_pointer, kFragment, C_row + 16));
+            FLOAT_4(fB[0]) = FLOAT_4(sB(shared_pointer, kFragment, C_col));
+            FLOAT_4(fB[4]) = FLOAT_4(sB(shared_pointer, kFragment, C_col + 32));
+
+            // compute outer product
+            #pragma unroll
+            for (int i=0; i<8;i++){
+                #pragma unroll
+                for (int j=0; j<8; j++) {
+                    accum[i*8+j] += fA[i] * fB[j];
+                }
+             }
+        }
+
+        // store to smem sA, sB for next block
+        if (kBlock < OC / TILE_WIDTH - 1) {
+
+
+            //FLOAT_4(sA[sA_sOffset]) = FLOAT_4(rA);
+            #pragma unroll
+            for (int i=0; i<4;i++){
+                sA(shared_pointer^1, sA_col + i, sA_row) = rA[i];
+                //sA[shared_pointer^1][sA_sOffset + i*BLOCK_WIDTH] = rA[i];
+            }
+
+            FLOAT_4(sB(shared_pointer^1, sB_row, sB_col)) = FLOAT_4(rB);
+
+            __syncthreads();
+
+            A += TILE_WIDTH;
+            B += TILE_WIDTH * C;
+
+            shared_pointer ^= 1;
+        }
+
+    }
+
+
+
+    inp = &inp((block_idx << 7), (block_idy << 7));
+
+// 3.37
+//    #pragma unroll
+//    for (int i=0;i<4;i+=2) {
+//        float reg_inp[32];
+//        FLOAT_4(reg_inp[0]) = FLOAT_4(inp(C_row + i, C_col));
+//        FLOAT_4(reg_inp[4]) = FLOAT_4(inp(C_row + i, C_col + 32));
+//        FLOAT_4(reg_inp[8]) = FLOAT_4(inp(C_row + i + 16, C_col));
+//        FLOAT_4(reg_inp[12]) = FLOAT_4(inp(C_row + i + 16, C_col + 32));
+//
+//        FLOAT_4(reg_inp[16]) = FLOAT_4(inp(C_row + i + 1, C_col));
+//        FLOAT_4(reg_inp[20]) = FLOAT_4(inp(C_row + i + 1, C_col + 32));
+//        FLOAT_4(reg_inp[24]) = FLOAT_4(inp(C_row + i + 1 + 16, C_col));
+//        FLOAT_4(reg_inp[28]) = FLOAT_4(inp(C_row + i + 1 + 16, C_col + 32));
+//        #pragma unroll
+//        for (int j = 0;j<4;j++) {
+//            epilogue_gelu_backward(&accum[i * 8 + j], reg_inp[j]);
+//            epilogue_gelu_backward(&accum[i * 8 + 4 + j], reg_inp[j + 4]);
+//            epilogue_gelu_backward(&accum[(i + 4) * 8 + j],  reg_inp[j + 8]);
+//            epilogue_gelu_backward(&accum[(i + 4) * 8 + 4 +j],  reg_inp[j + 12]);
+//
+//            epilogue_gelu_backward(&accum[(i + 1) * 8 + j], reg_inp[j + 16]);
+//            epilogue_gelu_backward(&accum[(i + 1) * 8 + 4 + j], reg_inp[j + 20]);
+//            epilogue_gelu_backward(&accum[(i + 1 + 4) * 8 + j],  reg_inp[j + 24]);
+//            epilogue_gelu_backward(&accum[(i + 1 + 4) * 8 + 4 +j],  reg_inp[j + 28]);
+//        }
+//        FLOAT_4(dinp(C_row + i, C_col)) = FLOAT_4(accum[i * 8]);
+//        FLOAT_4(dinp(C_row + i, C_col + 32)) = FLOAT_4(accum[i * 8 + 4]);
+//        FLOAT_4(dinp(C_row + i + 16, C_col)) = FLOAT_4(accum[(i+4) * 8]);
+//        FLOAT_4(dinp(C_row + i + 16, C_col + 32)) = FLOAT_4(accum[(i+4) * 8 + 4]);
+//
+//        FLOAT_4(dinp(C_row + i + 1, C_col)) = FLOAT_4(accum[(i + 1) * 8]);
+//        FLOAT_4(dinp(C_row + i + 1, C_col + 32)) = FLOAT_4(accum[(i + 1) * 8 + 4]);
+//        FLOAT_4(dinp(C_row + i + 1 + 16, C_col)) = FLOAT_4(accum[(i + 1+ 4) * 8]);
+//        FLOAT_4(dinp(C_row + i + 1 + 16, C_col + 32)) = FLOAT_4(accum[(i + 1+ 4) * 8 + 4]);
+//    }
+
+
+// 3.36
+    #pragma unroll
+    for (int i=0;i<4;i++) {
+        float reg_inp[16];
+        FLOAT_4(reg_inp[0]) = FLOAT_4(inp(C_row + i, C_col));
+        FLOAT_4(reg_inp[4]) = FLOAT_4(inp(C_row + i, C_col + 32));
+        FLOAT_4(reg_inp[8]) = FLOAT_4(inp(C_row + i + 16, C_col));
+        FLOAT_4(reg_inp[12]) = FLOAT_4(inp(C_row + i + 16, C_col + 32));
+        #pragma unroll
+        for (int j = 0;j<4;j++) {
+            epilogue_gelu_backward(&accum[i * 8 + j], reg_inp[j]);
+            epilogue_gelu_backward(&accum[i * 8 + 4 + j], reg_inp[j + 4]);
+            epilogue_gelu_backward(&accum[(i + 4) * 8 + j],  reg_inp[j + 8]);
+            epilogue_gelu_backward(&accum[(i + 4) * 8 + 4 +j],  reg_inp[j + 12]);
+        }
+        FLOAT_4(dinp(C_row + i, C_col)) = FLOAT_4(accum[i * 8]);
+        FLOAT_4(dinp(C_row + i, C_col + 32)) = FLOAT_4(accum[i * 8 + 4]);
+        FLOAT_4(dinp(C_row + i + 16, C_col)) = FLOAT_4(accum[(i+4) * 8]);
+        FLOAT_4(dinp(C_row + i + 16, C_col + 32)) = FLOAT_4(accum[(i+4) * 8 + 4]);
+    }
+
+}
+
 // ----------------------------------------------------------------------------
 // kernel launchers
 
@@ -1585,7 +1754,8 @@ void fused_matmul_gelu_backward(float* dinp, float* dweight, float* dbias,
 
     dim3 gridDim(B * T / 128, C / 128);
     dim3 blockDim(256);
-    fused_matmul_gelu_backward_kernel2<<<gridDim, blockDim>>>(dout, weight, dinp, pre_gelu_inp, B * T, C, OC);
+    //fused_matmul_gelu_backward_kernel2<<<gridDim, blockDim>>>(dout, weight, dinp, pre_gelu_inp, B * T, C, OC);
+    fused_matmul_gelu_backward_kernel3<<<gridDim, blockDim>>>(dout, weight, dinp, pre_gelu_inp, B * T, C, OC);
 
 
     // backward to weight, uses += in the backward pass (accumulate the gradient)
