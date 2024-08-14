ae885f8e079e265bac3ea7c3e3ffaf4e99fc4a4c
Steve Siu <steve.siu@oracle.com>
2024-08-14 14:34:27 +1000

    fix

--- dev/cuda/attention_forward.cu
+++ dev/cuda/attention_forward.cu
@@ -722,7 +722,7 @@ __global__ void flash_attention_forward_kernel0(float* q, float* k, float* v, fl
     int kv_offset = blockIdx.z * NH * T * HS + blockIdx.y * T * HS;
     q += q_offset;
     k += kv_offset;
-    v += q_offset;
+    v += kv_offset;
     vaccum += kv_offset;
     float q_reg[HS] = {0.0f};
     float x = 0.0f;
