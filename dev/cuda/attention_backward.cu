c53cc2bebc87e52ac77afe639ac42e7c55dcb952
Steve Siu <steve.siu@oracle.com>
2024-09-02 14:16:37 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -1306,7 +1306,7 @@ int main(int argc, char **argv) {
 
     // execute the forward pass on the GPU
     const int block_size = 256;
-    attention_forward(d_out, d_vaccum, d_qkvr, d_preatt, d_att, d_inp, B, T, C, NH, block_size);
+   // attention_forward(d_out, d_vaccum, d_qkvr, d_preatt, d_att, d_inp, B, T, C, NH, block_size);
     flash_attention_forward(d_out, d_inp, d_l, B, T, C, NH);
 
     // check that preatt, att, and out match between the CPU and GPU versions
@@ -1343,8 +1343,8 @@ int main(int argc, char **argv) {
 
     // call backward() on the GPU
 //    attention_backward(kernel_num, d_dinp, d_dqkvr, d_dpreatt, d_datt, d_dvaccum,
-                       d_dout, d_inp, d_qkvr, d_preatt, d_att, d_vaccum,
-                       B, T, C, NH, block_size);
+//                       d_dout, d_inp, d_qkvr, d_preatt, d_att, d_vaccum,
+//                       B, T, C, NH, block_size);
     flash_attention_backward(d_dinp, d_inp, d_dout, d_out, d_l, B, T, C, NH);
 
     // check that the gradients match between the CPU and GPU versions
