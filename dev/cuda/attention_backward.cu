113d1ab30e08f26bc0fa97c6b4e7e0a8b5cf7868
Steve Siu <steve.siu@oracle.com>
2024-09-13 16:03:54 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -427,8 +427,9 @@ __global__ void flash_attention_forward_kernel1(float* out, float* inp, float* l
 
 
             for (int i = 0; i < 4; i++) {
-                printf("address in sQ is %d, sQ is %f, rQ is %f\n", (warp_row + thread_row + i) * 64 + k_fragment, sQ(warp_row + thread_row + i, k_fragment), rQ[i]);
-                //rQ[i] = sQ(warp_row + thread_row + i, k_fragment);
+                printf("ORIGINAL address in sQ is %d, sQ is %f, rQ is %f\n", (warp_row + thread_row + i) * 64 + k_fragment, sQ(warp_row + thread_row + i, k_fragment), rQ[i]);
+                rQ[i] = sQ(warp_row + thread_row + i, k_fragment);
+                printf("UPDATE address in sQ is %d, sQ is %f, rQ is %f\n", (warp_row + thread_row + i) * 64 + k_fragment, sQ(warp_row + thread_row + i, k_fragment), rQ[i]);
                 //rQ[i] = 0;
                 rK[i] = sK(k_fragment, thread_col + i);
             }
