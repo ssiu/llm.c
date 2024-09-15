cfb7b0933db29cb085658e087d0a9031f1fc7560
Steve Siu <steve.siu@oracle.com>
2024-09-15 14:26:57 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -626,20 +626,20 @@ __global__ void flash_attention_forward_kernel1(float* out, float* inp, float* l
             }
         }
 
-//        if (blockIdx.y == 1 && kv_tile == 0 && (threadIdx.x == 0 || threadIdx.x == 16)){
-//            for (int i=0;i<4;i++){
-//                printf("kernel 1: t = 63, i = %d, m[i] = %f, l[i] = %f\n", (threadIdx.x / 16) * 4 + i + 64, rM[i], rL[i]);
-//            }
-//        }
+        if (blockIdx.y == 1 && kv_tile == 0 && (threadIdx.x == 0 || threadIdx.x == 16)){
+            for (int i=0;i<4;i++){
+                printf("kernel 1: t = 63, i = %d, m[i] = %f, l[i] = %f\n", (threadIdx.x / 16) * 4 + i + 64, rM[i], rL[i]);
+            }
+        }
 
         //each block compute 64 rows of o
     //each warp computes 8 rows, total 8 warps = 256 threads
     // print d when t = 64, ie after finishing the first block
-        if (blockIdx.y == 1 && kv_tile == 1 && (threadIdx.x == 0 || threadIdx.x == 16)){
-            for (int i=0;i<4;i++){
-                printf("kernel 1: t = 128, i = %d, m[i] = %f, l[i] = %f\n", (threadIdx.x / 16) * 4 + i + 64, rM[i], rL[i]);
-            }
-        }
+//        if (blockIdx.y == 1 && kv_tile == 1 && (threadIdx.x == 0 || threadIdx.x == 16)){
+//            for (int i=0;i<4;i++){
+//                printf("kernel 1: t = 128, i = %d, m[i] = %f, l[i] = %f\n", (threadIdx.x / 16) * 4 + i + 64, rM[i], rL[i]);
+//            }
+//        }
 
 
         // update m and l
