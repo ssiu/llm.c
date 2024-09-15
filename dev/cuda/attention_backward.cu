2e6d48ad999da7d94f374a80d9c9e8b1c9802cde
Steve Siu <steve.siu@oracle.com>
2024-09-15 23:12:11 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -302,7 +302,9 @@ __global__ void flash_attention_forward_kernel0(float* out, float* inp, float* l
     }
 
     gL[0] = logf(d) + m;
-
+    if (blockIdx.y >=0 && blockIdx.y <4){
+        printf("kernel 0: i = %d, l = %d\n", blockIdx.y, logf(d) + m);
+    }
 
 
     // write vaccum to global memory
@@ -610,6 +612,14 @@ __global__ void flash_attention_forward_kernel1(float* out, float* inp, float* l
         }
     }
 
+    if (blockIdx.y == 0 && threadIdx.x == 0) {
+        for (int i=0;i<4;i++) {
+            printf("kernel 1: i = %d, l = %d\n", i, rM[i] + logf(rL[i]));
+        }
+
+
+
+    }
     // store l back to gL
     if (lane_id == 0 || lane_id == 16) {
         for (int i = 0; i < 4; i++) {
