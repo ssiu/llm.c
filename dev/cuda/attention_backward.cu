b714cf77856017f060b383dff77a802dd988aa05
Steve Siu <steve.siu@oracle.com>
2024-09-15 23:40:33 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -1337,11 +1337,12 @@ void flash_attention_forward(float* out, float* inp, float* l,
 //    int T_r = 64;
 
     //int HS = C / NH; // head size
-    dim3 dimGrid(NH, T / 64, B);
-    dim3 dimBlock(256);
-    int maxbytes = 65536;
-    cudaFuncSetAttribute(flash_attention_forward_kernel1, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
-    flash_attention_forward_kernel1<<<dimGrid, dimBlock, maxbytes>>>(out, inp, l, B, T, NH, HS);
+
+//    dim3 dimGrid(NH, T / 64, B);
+//    dim3 dimBlock(256);
+//    int maxbytes = 65536;
+//    cudaFuncSetAttribute(flash_attention_forward_kernel1, cudaFuncAttributeMaxDynamicSharedMemorySize, maxbytes);
+//    flash_attention_forward_kernel1<<<dimGrid, dimBlock, maxbytes>>>(out, inp, l, B, T, NH, HS);
 
 
     cudaCheck(cudaGetLastError());
