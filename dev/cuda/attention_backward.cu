060a8153708d630c303812b6308ef5c9801cfcc1
Steve Siu <steve.siu@oracle.com>
2024-09-22 15:10:46 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -1251,18 +1251,11 @@ void flash_attention_forward_kernel3(float* out, float* inp, float* l,
                         rP[i] = __shfl_sync(mask, tP[i][k + h * 4], (lane_id /16) * 16  + ((l * 4 + k) / 4));
                         rP[i + 4] = __shfl_sync(mask, tP[i + 4][k + h * 4], (lane_id /16) * 16  + ((l * 4 + k) / 4));
                         rV[i] = sV(h * 64 + l * 4 + k, thread_col + i);
-//                        if (threadIdx.x == 0 && tile==0) {
-//                            printf("i = %d, rP[i] = %f, rP[i+4] = %f, rV[i] = %f\n", i, rP[i], rP[i+4], rV[i]);
-//                        }
                     }
 
                     for (int i = 0; i < 8; i++) {
                         for (int j = 0; j < 4; j++) {
-
                             rO[i][j] += rP[i] * rV[j];
-//                            if (threadIdx.x == 0 && tile==0) {
-//                                printf("i = %d, j= %d, rO[i][j] = %f \n", i, j, rO[i][j]);
-//                            }
                         }
                     }
                 }
