9a0e9b72bb4ab585854014b6ba5995f0028ddacd
steve <stevesiu1013@gmail.com>
2024-10-02 11:34:10 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -1499,7 +1499,7 @@ __global__ void flash_attention_backward_preprocessing_kernel1(float* d, float* 
 #define sP_T(i,j) sP[(i) + (j) * TILE_SIZE]
 #define sdS(i,j) sdS[(i) * TILE_SIZE + (j)]
 #define sdS_T(i,j) sdS[(i) + (j) * TILE_SIZE]
-#define FLOAT4(value) *reinterpret_cast<float4*>(&(value))
+#define FLOAT4(value) *reinterpret_cast<float4*>(&(value))[0]
 
 __global__ void flash_attention_backward_kernel1(float* dinp, float* inp, float* dout, float* out, float* dl, float* dd,
                                 int B, int T, int NH, int HS) {
@@ -1744,7 +1744,7 @@ __global__ void flash_attention_backward_kernel1(float* dinp, float* inp, float*
 
             for (int i=0;i<4;i++) {
                 for (int j=0; j<4; j++) {
-                    rdK[i][j] += rdS[i] * rQ[j];
+                    tdK[i][j] += rdS[i] * rQ[j];
                 }
             }
         }
@@ -1761,14 +1761,14 @@ __global__ void flash_attention_backward_kernel1(float* dinp, float* inp, float*
 
             for (int i=0;i<4;i++) {
                 for (int j=0; j<4; j++) {
-                    rdQ[i][j] += rdS[i] * rK[j];
+                    tdQ[i][j] += rdS[i] * rK[j];
                 }
             }
         }
 
         for (int i=0;i<4;i++) {
             for (int j=0; j<4; j++) {
-                rdQ[i][j] *= 1.0f / sqrtf(HS);
+                tdQ[i][j] *= 1.0f / sqrtf(HS);
             }
         }
 
