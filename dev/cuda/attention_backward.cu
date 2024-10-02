07bfffc5e7f1ed4f6a708d71ad67044374e1128c
steve <stevesiu1013@gmail.com>
2024-10-02 11:32:46 +1000

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -1499,7 +1499,7 @@ __global__ void flash_attention_backward_preprocessing_kernel1(float* d, float* 
 #define sP_T(i,j) sP[(i) + (j) * TILE_SIZE]
 #define sdS(i,j) sdS[(i) * TILE_SIZE + (j)]
 #define sdS_T(i,j) sdS[(i) + (j) * TILE_SIZE]
-//#define FLOAT4(value) *reinterpret_cast<float4*>(&(value))
+#define FLOAT4(value) *reinterpret_cast<float4*>(&(value))
 
 __global__ void flash_attention_backward_kernel1(float* dinp, float* inp, float* dout, float* out, float* dl, float* dd,
                                 int B, int T, int NH, int HS) {
