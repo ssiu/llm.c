fd594bc18130a1d4d27f65f368186731c21ba91b
steve <stevesiu1013@gmail.com>
2024-10-14 00:03:46 +1100

    fix

--- dev/cuda/attention_backward.cu
+++ dev/cuda/attention_backward.cu
@@ -2314,6 +2314,18 @@ __global__ void flash_attention_backward_kernel2(float* dinp, float* inp, float*
             }
         }
 
+        if (blockIdx.y == 0 && thread_id == 0) {
+            printf("kernel 2\n");
+            for (int i=0;i<4;i++) {
+                for (int j=0;j<4;j++) {
+                    printf("%f ", tdV[i][j]);
+                }
+                printf("\n");
+            }
+            printf("==========\n");
+        }
+
+
         __syncthreads();
 
         //
@@ -2765,7 +2777,16 @@ void flash_attention_backward_kernel3(float* dinp, float* inp, float* dout, floa
             }
         }
 
-
+        if (blockIdx.y == 0 && thread_id == 0) {
+            printf("kernel 3\n");
+            for (int i=0;i<4;i++) {
+                for (int j=0;j<4;j++) {
+                    printf("%f ", tdV[i][j]);
+                }
+                printf("\n");
+            }
+            printf("==========\n");
+        }
 
 
         //
